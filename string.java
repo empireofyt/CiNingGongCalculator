7977903a44e0951eb8f63f0fa113dd711dc6079a
zhangyangping321 <46313570+zhangyangping321@users.noreply.github.com>
2019-01-04 19:19:30 +0800

    Add files via upload
    onclick-字符串

---
+++ string.java
@@ -0,0 +1,23 @@
+/**
+                 * 字符串长度大于 0 时才截取字符串
+                 * 如果长度为 1，则直接把字符串设置为 0
+                 */
+                if (existedText.equals("error")){
+                    existedText = "0";
+                } else if (existedText.length() > 0){
+                    if (existedText.length() == 1) {
+                        existedText = "0";
+                    } else {
+                        existedText = existedText.substring(0,existedText.length()-1);
+                    }
+                }
+                break;
+            case R.id.ac_btn:
+                existedText = "0";
+                break;
+        }
+        /**
+         * 设置显示
+         */
+        mResultText.setText(existedText);
+   
