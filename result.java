730d1e0f85edc07393bf2ee52b0882b997b4e3c1
LeeWenJuan <46313424+LeeWenJuan@users.noreply.github.com>
2019-01-04 19:33:07 +0800

    onclick-result
    onclick-result

---
+++ result.java
@@ -0,0 +1,23 @@
+ /**
+     * 进行运算，得到结果
+     * @return  返回结果
+     */
+    private String getResult() {
+
+        /**
+         * 结果
+         */
+        String tempResult = null;
+        /**
+         * 两个String类型的参数
+         */
+        String param1 = null;
+        String param2 = null;
+        /**
+         * 转换后的两个double类型的参数
+         */
+        double arg1 = 0;
+        double arg2 = 0;
+        double result = 0;
+
+        getCondition();
