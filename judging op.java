fb3a2566fed57f85ea288961168a9c6b6a824839
qingjiuxdd <46313386+qingjiuxdd@users.noreply.github.com>
2019-01-04 19:37:30 +0800

    onclick-judging op
    判断是否运算过，若无则判断运算符及数字

---
+++ judging op.java
@@ -0,0 +1,66 @@
+case R.id.dot_btn:
+                /**
+                 * 判断是否运算过
+                 * 否
+                 *   判断是否有运算符，有 判断运算符之后的数字，无 判断整个数字
+                 *   判断数字是否过长，是则不能添加小数点，否则可以添加
+                 *   判断已经存在的数字里是否有小数点
+                 * 是
+                 *   字符串置为 0.
+                 */
+                if (!isCounted){
+
+                    if (existedText.contains("+") || existedText.contains("-") ||
+                            existedText.contains("×") || existedText.contains("÷") ){
+
+                        String param1 = null;
+                        String param2 = null;
+
+                        if (existedText.contains("+")) {
+                            param1 = existedText.substring(0, existedText.indexOf("+"));
+                            param2 = existedText.substring(existedText.indexOf("+") + 1);
+                        } else if (existedText.contains("-")) {
+                            param1 = existedText.substring(0, existedText.indexOf("-"));
+                            param2 = existedText.substring(existedText.indexOf("-") + 1);
+                        } else if (existedText.contains("×")) {
+                            param1 = existedText.substring(0, existedText.indexOf("×"));
+                            param2 = existedText.substring(existedText.indexOf("×") + 1);
+                        } else if (existedText.contains("÷")) {
+                            param1 = existedText.substring(0, existedText.indexOf("÷"));
+                            param2 = existedText.substring(existedText.indexOf("÷") + 1);
+                        }
+                        Log.d("Anonymous param1",param1);
+                        Log.d("Anonymous param2",param2);
+
+                        boolean isContainedDot = param2.contains(".");
+                        if (param2.length() >= 9){
+
+                        } else if (!isContainedDot){
+                            if (param2.equals("")){
+                                existedText += "0.";
+                            } else {
+                                existedText += ".";
+                            }
+                        } else {
+                            return;
+                        }
+                    } else {
+                        boolean isContainedDot = existedText.contains(".");
+                        if (existedText.length() >= 9){
+
+                        } else if (!isContainedDot){
+                            existedText += ".";
+                        } else {
+                            return;
+                        }
+                    }
+                    isCounted = false;
+
+                } else {
+                    existedText = "0.";
+                    isCounted = false;
+                }
+
+
+                break;
+            case R.id.percent_btn:
