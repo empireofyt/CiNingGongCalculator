47180a74ce5131215e691fb1ff6887a0bae22736
qingjiuxdd <46313386+qingjiuxdd@users.noreply.github.com>
2019-01-04 19:48:22 +0800

    onclick-op judgment
     如果有运算符，则进行运算,没有运算符，则把已经存在的数据再传出去

---
+++ operational judgment1.java
@@ -0,0 +1,85 @@
+/**
+         * 如果有运算符，则进行运算
+         * 没有运算符，则把已经存在的数据再传出去
+         */
+        if ( startWithOperator || noStartWithOperator || startWithSubtract) {
+
+            if (existedText.contains("+")) {
+                /**
+                 * 先获取两个参数
+                 */
+                param1 = existedText.substring(0, existedText.indexOf("+"));
+                param2 = existedText.substring(existedText.indexOf("+") + 1);
+                /**
+                 * 如果第二个参数为空，则还是显示当前字符
+                 */
+                if (param2.equals("")){
+                    tempResult = existedText;
+                } else {
+                    /**
+                     * 转换String为Double
+                     * 计算后再转换成String类型
+                     * 进行正则表达式处理
+                     */
+                    arg1 = Double.parseDouble(param1);
+                    arg2 = Double.parseDouble(param2);
+                    result = arg1 + arg2;
+                    tempResult = String.format("%f", result);
+                    tempResult = subZeroAndDot(tempResult);
+                }
+
+
+            } else if (existedText.contains("×")) {
+
+                param1 = existedText.substring(0, existedText.indexOf("×"));
+                param2 = existedText.substring(existedText.indexOf("×") + 1);
+
+                if (param2.equals("")){
+                    tempResult = existedText;
+                } else {
+                    arg1 = Double.parseDouble(param1);
+                    arg2 = Double.parseDouble(param2);
+                    result = arg1 * arg2;
+                    tempResult = String.format("%f", result);
+                    tempResult = subZeroAndDot(tempResult);
+                }
+
+            } else if (existedText.contains("÷")) {
+
+                param1 = existedText.substring(0, existedText.indexOf("÷"));
+                param2 = existedText.substring(existedText.indexOf("÷") + 1);
+
+                if (param2.equals("0")){
+                    tempResult = "error";
+                } else if (param2.equals("")){
+                    tempResult = existedText;
+                } else {
+                    arg1 = Double.parseDouble(param1);
+                    arg2 = Double.parseDouble(param2);
+                    result = arg1 / arg2;
+                    tempResult = String.format("%f", result);
+                    tempResult = subZeroAndDot(tempResult);
+                }
+
+            } else if (existedText.contains("-")) {
+
+                /**
+                 * 这里是以最后一个 - 号为分隔去取出两个参数
+                 * 进到这个方法，必须满足有运算公式
+                 * 而又避免了第一个参数是负数的情况
+                 */
+                param1 = existedText.substring(0, existedText.lastIndexOf("-"));
+                param2 = existedText.substring(existedText.lastIndexOf("-") + 1);
+
+                if (param2.equals("")){
+                    tempResult = existedText;
+                } else {
+                    arg1 = Double.parseDouble(param1);
+                    arg2 = Double.parseDouble(param2);
+                    result = arg1 - arg2;
+                    tempResult = String.format("%f", result);
+                    tempResult = subZeroAndDot(tempResult);
+                }
+
+            }
+           
