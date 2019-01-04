9449724490b4d54839dd94c438d48551c507c92f
LeeWenJuan <46313424+LeeWenJuan@users.noreply.github.com>
2019-01-04 19:32:26 +0800

    onclick-operator
    onclick-operator

---
+++ judge operational.java
@@ -0,0 +1,48 @@
+/**
+             * 运算符
+             */
+            case R.id.plus_btn:
+                /**
+                 * 判断已有的字符是否是科学计数
+                 * 是 置零
+                 * 否 进行下一步
+                 *
+                 * 判断表达式是否可以进行计算
+                 * 是 先计算再添加字符
+                 * 否 添加字符
+                 *
+                 * 判断计算后的字符是否是 error
+                 * 是 置零
+                 * 否 添加运算符
+                 */
+                if (!existedText.contains("e")) {
+
+                    if (judgeExpression()) {
+                        existedText = getResult();
+                        if (existedText.equals("error")){
+
+                        } else {
+                            existedText += "+";
+                        }
+                    } else {
+
+                        if (isCounted) {
+                            isCounted = false;
+                        }
+
+                        if ((existedText.substring(existedText.length() - 1)).equals("-")) {
+                            existedText = existedText.replace("-", "+");
+                        } else if ((existedText.substring(existedText.length() - 1)).equals("×")) {
+                            existedText = existedText.replace("×", "+");
+                        } else if ((existedText.substring(existedText.length() - 1)).equals("÷")) {
+                            existedText = existedText.replace("÷", "+");
+                        } else if (!(existedText.substring(existedText.length() - 1)).equals("+")) {
+                            existedText += "+";
+                        }
+                    }
+                } else {
+                    existedText = "0";
+                }
+break;
+           
+
