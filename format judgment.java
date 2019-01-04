b6fe7dc406bf8c20ef1d8bc94be04ec1dc119766
Wingswag <46313367+Wingswag@users.noreply.github.com>
2019-01-04 19:56:44 +0800

    数字格式
    onclick--数字格式

---
+++ format judgment.java
@@ -0,0 +1,120 @@
+
+   
+/**
+     * 先判断是否按过等于号
+     * 是 按数字则显示当前数字
+     * 否 在已有的表达式后添加字符
+     *
+     * 判断数字是否就是一个 0
+     * 是 把字符串设置为空字符串。
+     *   1、打开界面没有运算过的时候，AC键归零或删除完归零的时候，会显示一个 0
+     *   2、当数字是 0 的时候，设置成空字符串，再按 0 ，数字会还是 0，不然可以按出 000 这种数字
+     * 否 添加按下的键的字符
+     *
+     * 判断数字是否包含小数点
+     * 是 数字不能超过十位
+     * 否 数字不能超过九位
+     *
+     * 进行上面的判断后，再判断数字是否超过长度限制
+     * 超过不做任何操作
+     * 没超过可以再添数字
+     */
+    private String isOverRange(String existedText, String s) {
+        /**
+         * 判断是否计算过
+         */
+        if (!isCounted){
+            /**
+             * 判断是否是科学计数
+             * 是 文本置零
+             */
+            if (existedText.contains("e")){
+                existedText = "0";
+            }
+            /**
+             * 判断是否只有一个 0
+             * 是 文本清空
+             */
+            if (existedText.equals("0")){
+                existedText = "";
+            }
+            /**
+             * 判断是否有运算符
+             * 是 判断第二个数字
+             * 否 判断整个字符串
+             */
+            if (existedText.contains("+") || existedText.contains("-") ||
+                    existedText.contains("×") || existedText.contains("÷")){
+                /**
+                 * 包括运算符时 两个数字 判断第二个数字
+                 * 两个参数
+                 */
+                String param2 = null;
+                if (existedText.contains("+")){
+                    param2 = existedText.substring(existedText.indexOf("+")+1);
+                } else if (existedText.contains("-")){
+                    param2 = existedText.substring(existedText.indexOf("-")+1);
+                } else if (existedText.contains("×")){
+                    param2 = existedText.substring(existedText.indexOf("×")+1);
+                } else if (existedText.contains("÷")){
+                    param2 = existedText.substring(existedText.indexOf("÷")+1);
+                }
+
+//            Log.d("Anonymous param1",param1);
+//            Log.d("Anonymous param2",param2);
+             if (existedText.substring(existedText.length()-1).equals("+") ||
+                        existedText.substring(existedText.length()-1).equals("-") ||
+                        existedText.substring(existedText.length()-1).equals("×") ||
+                        existedText.substring(existedText.length()-1).equals("÷")){
+                    existedText += s;
+                } else {
+                    if (param2.contains(".")){
+                        if (param2.length() >= 10){
+
+                        } else {
+                            existedText += s;
+                        }
+                    } else {
+                        if (param2.length() >= 9){
+
+                        } else {
+                            existedText += s;
+                        }
+                    }
+                }
+            } else {
+                /**
+                 * 不包括运算符时 一个数字
+                 */
+                if (existedText.contains(".")){
+                    if (existedText.length() >= 10){
+
+                    } else {
+                        existedText += s;
+                    }
+                } else {
+                    if (existedText.length() >= 9){
+
+                    } else {
+                        existedText += s;
+                    }
+                }
+            }
+
+            isCounted = false;
+
+        } else {
+
+            existedText = s;
+            isCounted = false;
+
+        } 
+
+
+        return existedText;
+    }
+   
+
+
+        return existedText;
+    }
