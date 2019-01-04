e850878e09578e6170e3ef4070b74d3a4ba0185d
LeeWenJuan <46313424+LeeWenJuan@users.noreply.github.com>
2019-01-04 19:30:05 +0800

    onclick-existed
    onclick-existed

---
+++ judge existed.java
@@ -0,0 +1,83 @@
+ case R.id.subtract_btn:
+
+                if (!existedText.contains("e")) {
+                    if (judgeExpression()) {
+                        existedText = getResult();
+                        if (existedText.equals("error")){
+
+                        } else {
+                            existedText += "-";
+                        }
+                    } else {
+
+                        if (isCounted) {
+                            isCounted = false;
+                        }
+
+                        if ((existedText.substring(existedText.length() - 1)).equals("+")) {
+//                    Log.d("Anonymous", "onClick: " + "进入减法方法");
+                            existedText = existedText.replace("+", "-");
+                        } else if ((existedText.substring(existedText.length() - 1)).equals("×")) {
+                            existedText = existedText.replace("×", "-");
+                        } else if ((existedText.substring(existedText.length() - 1)).equals("÷")) {
+                            existedText = existedText.replace("÷", "-");
+                        } else if (!(existedText.substring(existedText.length() - 1)).equals("-")) {
+                            existedText += "-";
+                        }
+                    }
+                } else {
+                    existedText = "0";
+                }
+                break;
+            case R.id.multiply_btn:
+
+                if (!existedText.contains("e")) {
+                    if (judgeExpression()) {
+                        existedText = getResult();
+                        if (existedText.equals("error")){
+
+                        } else {
+                            existedText += "×";
+                        }
+
+                    } else {
+
+                        if (isCounted) {
+                            isCounted = false;
+                        }
+
+                        if ((existedText.substring(existedText.length() - 1)).equals("+")) {
+                            existedText = existedText.replace("+", "×");
+                        } else if ((existedText.substring(existedText.length() - 1)).equals("-")) {
+                            existedText = existedText.replace("-", "×");
+                        } else if ((existedText.substring(existedText.length() - 1)).equals("÷")) {
+                            existedText = existedText.replace("÷", "×");
+                        } else if (!(existedText.substring(existedText.length() - 1)).equals("×")) {
+                            existedText += "×";
+                        }
+                    }
+                } else {
+                    existedText = "0";
+                }
+                break;
+            case R.id.divide_btn:
+
+                if (!existedText.contains("e")) {
+                    if (judgeExpression()) {
+                        existedText = getResult();
+                        if (existedText.equals("error")){
+
+                        } else {
+                            existedText += "÷";
+                        }
+
+                    } else {
+
+                        if (isCounted) {
+                            isCounted = false;
+                        }
+
+                        if ((existedText.substring(existedText.length() - 1)).equals("+")) {
+                            existedText = existedText.replace("+", "÷");
+                        } else if ((existedText.substring(existedText.length() - 1)).equals("-")) {
+                            e
