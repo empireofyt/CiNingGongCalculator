124ae94f9117a2f212103c5986b192a2b8699587
LeeWenJuan <46313424+LeeWenJuan@users.noreply.github.com>
2019-01-04 19:31:38 +0800

    onclick-expression
    onclick-expression

---
+++ judge expression.java
@@ -0,0 +1,66 @@
+/**
+     * 判断表达式
+     *
+     * 为了按等号是否进行运算
+     * 以及出现两个运算符（第一个参数如果为负数的符号不计）先进行运算再添加运算符
+     */
+    private boolean judgeExpression() {
+
+        getCondition();
+
+        String tempParam2 = null;
+
+        if ( startWithOperator || noStartWithOperator || startWithSubtract) {
+
+            if (existedText.contains("+")) {
+                /**
+                 * 先获取第二个参数
+                 */
+                tempParam2 = existedText.substring(existedText.indexOf("+") + 1);
+                /**
+                 * 如果第二个参数为空，表达式不成立
+                 */
+                if (tempParam2.equals("")) {
+                    return false;
+                } else {
+                    return true;
+                }
+            } else if (existedText.contains("×")) {
+
+                tempParam2 = existedText.substring(existedText.indexOf("×") + 1);
+
+                if (tempParam2.equals("")) {
+                    return false;
+                } else {
+                    return true;
+                }
+
+            } else if (existedText.contains("÷")) {
+
+                tempParam2 = existedText.substring(existedText.indexOf("÷") + 1);
+
+                if (tempParam2.equals("")) {
+                    return false;
+                } else {
+                    return true;
+                }
+
+            } else if (existedText.contains("-")) {
+
+                /**
+                 * 这里是以最后一个 - 号为分隔去取出两个参数
+                 * 进到这个方法，必须满足有运算公式
+                 * 而又避免了第一个参数是负数的情况
+                 */
+                tempParam2 = existedText.substring(existedText.lastIndexOf("-") + 1);
+
+                if (tempParam2.equals("")) {
+                    return false;
+                } else {
+                    return true;
+                }
+
+            }
+        }
+        return false;
+    }
