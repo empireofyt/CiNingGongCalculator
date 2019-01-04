27e3b7774f015d37c1c89b56c4425a6cae304704
zhangyangping321 <46313570+zhangyangping321@users.noreply.github.com>
2019-01-04 19:15:04 +0800

    Add files via upload
    onclick-数字

---
+++ number.java
@@ -0,0 +1,33 @@
+/**
+             * Êý×Ö
+             */
+            case R.id.num_zero:
+                existedText = isOverRange(existedText,"0");
+                break;
+            case R.id.num_one:
+                existedText = isOverRange(existedText,"1");
+                break;
+            case R.id.num_two:
+                existedText = isOverRange(existedText,"2");
+                break;
+            case R.id.num_three:
+                existedText = isOverRange(existedText,"3");
+                break;
+            case R.id.num_four:
+                existedText = isOverRange(existedText,"4");
+                break;
+            case R.id.num_five:
+                existedText = isOverRange(existedText,"5");
+                break;
+            case R.id.num_six:
+                existedText = isOverRange(existedText,"6");
+                break;
+            case R.id.num_seven:
+                existedText = isOverRange(existedText,"7");
+                break;
+            case R.id.num_eight:
+                existedText = isOverRange(existedText,"8");
+                break;
+            case R.id.num_nine:
+                existedText = isOverRange(existedText,"9");
+                break;
