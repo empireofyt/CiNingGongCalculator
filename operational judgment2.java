1bc3382c0810ee35f0a64a30eb11069ec3154409
qingjiuxdd <46313386+qingjiuxdd@users.noreply.github.com>
2019-01-04 19:51:48 +0800

    onclick-op judgment2
    判断数据长度，如果数据长度大于等于10位，进行科学计数，如果有小数点，再判断小数点前是否有十个数字，有则进行科学计数

---
+++ operational judgment2.java
@@ -0,0 +1,18 @@
+  /**
+             * 如果数据长度大于等于10位，进行科学计数
+             *
+             * 如果有小数点，再判断小数点前是否有十个数字，有则进行科学计数
+             */
+            if (tempResult.length() >= 10) {
+                tempResult = String.format("%e", Double.parseDouble(tempResult));
+            } else if (tempResult.contains(".")) {
+                if (tempResult.substring(0, tempResult.indexOf(".")).length() >= 10) {
+                    tempResult = String.format("%e", Double.parseDouble(tempResult));
+                }
+            }
+        } else {
+            tempResult = existedText;
+        }
+
+        return tempResult;
+    }
